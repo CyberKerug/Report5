891354d5d095f69bf4a792df8a994e76c06131e3
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-21 00:54:35 +0900

    1st Status.

---
+++ src/jp/ac/uryukyu/ie/e165735/StrError.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165735;
+
+public class StrError {
+    public static void main(String[] args) {
+        String str;
+        str = null;
+        int l;
+        l = str.length();
+        System.out.println("strに格納されている文字は" + l + "文字です。");
+    }
+}
