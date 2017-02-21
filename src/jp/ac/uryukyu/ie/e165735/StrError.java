75edc65bed9b42b2f474133a4c9d2b75dca4136c
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-21 13:55:02 +0900

    Format exception caught.

--- src/jp/ac/uryukyu/ie/e165735/StrError.java
+++ src/jp/ac/uryukyu/ie/e165735/StrError.java
@@ -6,10 +6,11 @@ public class StrError {
     public static void main(String[] args) {
         try {
             String str;
-            str = null;
+            str = "3．14";
             int l;
             l = str.length();
             System.out.println("strに格納されている文字は" + l + "文字です。");
+            System.out.println("strに格納されている数値は"+Double.parseDouble(str)+"です");
         }catch (NullPointerException e) {
             System.out.println("NullPointerExceptionが発生したため、\n strには何も格納されていません。");
             printStackTrace();
